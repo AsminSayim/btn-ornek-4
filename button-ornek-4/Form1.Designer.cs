b3eee1bcab4932f5d0b5a9e5760cc29e4313368d
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-10-10 16:39:35 +0300

    Add files via upload

---
+++ button-ornek-4/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace button_ornek_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTikla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTikla
+            // 
+            this.btnTikla.Location = new System.Drawing.Point(119, 70);
+            this.btnTikla.Name = "btnTikla";
+            this.btnTikla.Size = new System.Drawing.Size(197, 63);
+            this.btnTikla.TabIndex = 0;
+            this.btnTikla.Text = "TIKLA";
+            this.btnTikla.UseVisualStyleBackColor = true;
+            this.btnTikla.Click += new System.EventHandler(this.btnTikla_Click);
+            this.btnTikla.MouseEnter += new System.EventHandler(this.btnTikla_MouseEnter);
+            this.btnTikla.MouseLeave += new System.EventHandler(this.btnTikla_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 223);
+            this.Controls.Add(this.btnTikla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTikla;
+    }
+}
+
---
+++ button-ornek-4/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button_ornek_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTikla_MouseEnter(object sender, EventArgs e)
+        {
+            btnTikla.BackColor = Color.Blue;
+            btnTikla.ForeColor = Color.White;
+        }
+
+        private void btnTikla_MouseLeave(object sender, EventArgs e)
+        {
+            btnTikla.BackColor= Color.Red;
+            btnTikla.ForeColor = Color.White;
+        }
+
+        private void btnTikla_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nesne Tabanlı Programlama");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnTikla.BackColor = Color.Red;
+            btnTikla.ForeColor = Color.White;
+        }
+    }
+}
